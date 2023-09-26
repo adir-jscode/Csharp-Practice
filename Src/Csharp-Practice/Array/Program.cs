037fa5fc2660f4b557a87337b3318b5dfd311905
Adir <adir.jscode@gmail.com>
2023-09-26 19:59:47 +0600

    Jageed Array added

--- Src/Csharp-Practice/Array/Program.cs
+++ Src/Csharp-Practice/Array/Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Array
+{
+    class Program
+    {
+        static void Main(string[]args)
+        {
+            int[] arr = new int[4];
+
+            arr[0] = 10;
+            arr[1] = 20;
+            arr[2] = 30;
+            arr[3] = 40;
+
+            Console.WriteLine(arr[0]);
+
+
+            string[] names = { "Hasan", "Hossain","Ali","Adir","Sarafat" };
+
+            Console.WriteLine(names[1]);
+
+            for(int i=0;i<arr.Length;i++)
+            {
+                Console.WriteLine("Index NO " + i + " = " + arr[i]);
+            }
+
+            for(int i=0; i<names.Count(); i++)
+            {
+                Console.WriteLine("Person No " + (i + 1) + " = " + names[i]);
+            }
+
+            foreach(string namelist in names)
+            {
+                Console.WriteLine(namelist);
+            }
+
+
+
+        }
+    }
+}
