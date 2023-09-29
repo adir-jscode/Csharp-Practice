c9c167e2293c73c24f1b829d486e291d12bcc294
Adir <adir.jscode@gmail.com>
2023-09-30 01:57:30 +0600

    Updated

--- Src/Csharp-Practice/Multidimentional_Array/Program.cs
+++ Src/Csharp-Practice/Multidimentional_Array/Program.cs
@@ -15,19 +15,17 @@ namespace Multidimentional_Array
                 { 15,25,35,45}
             };
 
-            for(int i = 0; i < arr.GetLength(0); i++)
+            for(int i = 0; i < arr.GetLength(0); i++) //ROW
             {
-                for(int j = 0; j < arr.GetLength(1); j++)
+                for(int j = 0; j < arr.GetLength(1); j++) // Column
                 {
                     Console.WriteLine(arr[i,j] + " " + arr[i,j] * arr[i,j] );
                 }
             }
 
-            Console.WriteLine("Array Dimention is " + arr.Rank);
+            Console.WriteLine("Array Dimention is " + arr.Rank); // Returns diamension
 
-            int[][][] jagged = new int[3][][];
-
-            Console.WriteLine("Dimention is "+ jagged.Rank);
+           
 
 
 
